1d00a95819ac7bf7c801ed79458935f42274a4fa
blackpig <15184486929@163.com>
2021-09-20 22:59:01 +0800

    feat: 完成cli，自动路由

---
+++ node/vue-auto-router-cli/lib/refresh.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+const handlebars = require('handlebars')
+const chalk = require('chalk')
+
+module.exports = async () => {
+  //获取列表
+  const list = fs.readFileSync('./src/views')
+                .filter(v => v !== 'Home.vue')
+                .map(v => ({
+                  name: v.replace('.vue','').toLowerCase(),
+                  file: v
+                }))
+  //生成路由定义
+  compile({list},'./src/router.js','./template/router.js.hbs')
+  //生成菜单
+  compile({list},'./src/App.vue','./template/App.vue.hbs')
+
+  function compile(meta,filePath,templatePath) {
+    if(fs.existsSync(templatePath)) {
+      const content = fs.readFileSync(templatePath).toString()
+      const result = handlebars.compile(content)(meta)
+      fs.writeFileSync(filePath,result)
+      console.log(chalk.green(`${filePath} 创建成功`));
+    }
+  }
+}
